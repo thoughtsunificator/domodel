6b367b95ae927206940bf67ef2f0624cfb92be61
thoughtsunificator <oss@thoughtsunificator.me>
2021-10-03 12:51:42 +0200

    clean up

--- test/event-listener.js
+++ test/event-listener.js
@@ -1,6 +1,6 @@
 import assert from "assert"
 
-import { Observable, Binding, EventListener } from "../index.js"
+import { Observable, EventListener } from "../index.js"
 
 describe("EventListener", () => {
 
@@ -8,12 +8,10 @@ describe("EventListener", () => {
 		const observable = new Observable()
 		const eventListener = new EventListener(observable)
 		assert.strictEqual(eventListener.observable, observable)
-		const binding = new Binding()
 		const eventListener_ = new EventListener(observable)
 		assert.strictEqual(eventListener_.observable, observable)
 		assert.throws(() => {
 			eventListener_.observable = ""
-			eventListener_.binding = ""
 		})
 	})
 
